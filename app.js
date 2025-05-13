04c58794220efdba780255029c8f2f9f6129ce07
Simon Friis Hansen <30860295+simonfriishansen@users.noreply.github.com>
2025-05-13 13:53:31 +0200

    Use proper route, make return format easier to work with

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+// Mock fuel prices
+const fuelPrices = {
+	date: "2025-05-13",
+	prices: {
+		"Blyfri 95": 13.94,
+		"Oktan 100": 15.56,
+		Diesel: 12.49,
+	},
+};
+
+app.get("/api/fuel-prices", (req, res) => {
+	res.json(fuelPrices);
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`TripCost API listening on port ${port}`);
 });
